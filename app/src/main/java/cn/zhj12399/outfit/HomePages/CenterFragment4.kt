d139f20855d1cf43bdd2cda23124bdbc333a014e
O(∩_∩)O <zhj727534681@163.com>
2024-04-28 16:58:12 +0800

    2024-4-28

--- app/src/main/java/cn/zhj12399/outfit/HomePages/CenterFragment4.kt
+++ app/src/main/java/cn/zhj12399/outfit/HomePages/CenterFragment4.kt
@@ -1,66 +1,67 @@
 package cn.zhj12399.outfit.HomePages
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import cn.zhj12399.outfit.LoginPages.LoginActivity
 import cn.zhj12399.outfit.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [CenterFragment4.newInstance] factory method to
- * create an instance of this fragment.
- */
 class CenterFragment4 : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        val textview_username = view?.findViewById<TextView>(R.id.textview_username)
-        val PREF_FILE_NAME = "user_info"
-        val USER_NAME = "user_name"
-        val pref = getActivity()?.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
-        val user_name = pref?.getString(USER_NAME,"").toString()
-        requireActivity().runOnUiThread {
-            textview_username?.setText("您好，$user_name")
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val root =  inflater.inflate(R.layout.fragment_center4, container, false)
+        val root = inflater.inflate(R.layout.fragment_center4, container, false)
+        val textview_username = root.findViewById<TextView>(R.id.textview_username)
+        val button_log_off = root.findViewById<Button>(R.id.button_log_off)
+        val button_about = root.findViewById<Button>(R.id.button_about)
+        val PREF_FILE_NAME = "user_info"
+        val USER_NAME = "user_name"
+        val pref = getActivity()?.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE)
+        val user_name = pref?.getString(USER_NAME, "").toString()
+        textview_username?.setText("您好，$user_name")
 
-        return root
-    }
+        button_log_off.setOnClickListener {
+            MaterialAlertDialogBuilder(requireActivity())
+                .setTitle("确定注销账户码？")
+                .setMessage("退出后您的账号仍会保留\n其它信息也不会被删除")
+                .setNeutralButton("取消") { dialog, which ->
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment CenterFragment4.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            CenterFragment4().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
                 }
-            }
+                .setPositiveButton("是") { dialog, which ->
+                    val editor = pref?.edit()
+                    editor?.putString(USER_NAME, "")
+                    editor?.apply()
+
+                    Toast.makeText(activity, "成功下线", Toast.LENGTH_SHORT).show()
+                    val intent = Intent(activity, LoginActivity::class.java)
+                        .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                    activity?.startActivity(intent)
+                }
+                .show()
+        }
+        button_about.setOnClickListener {
+            MaterialAlertDialogBuilder(requireActivity())
+                .setTitle("关于穿搭世界")
+                .setMessage(
+                    "一款可记录每日穿搭的网站\n" +
+                            "作者:zhj12399\n" +
+                            "作者邮箱:zhj@bit.edu.cn\n" +
+                            "网站地址:https://ty.zhj12399.cn:9000"
+                )
+                .setPositiveButton("确定") { dialog, which ->
+                }
+                .show()
+        }
+        return root
     }
 }
